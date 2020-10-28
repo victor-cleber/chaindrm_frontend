320d6e9b3dab3a0c66a7d871040779f865dc77e5
Victor Cleber <victor.cleber@outlook.com>
2020-10-28 21:14:36 +1300

    Configurating the style and headers

--- web/settings.py
+++ web/settings.py
@@ -118,7 +118,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
